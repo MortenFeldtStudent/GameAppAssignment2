65934e4761fc073335a64a456565ef8c19f47e13
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2019-08-26 12:54:24 +0200

    GameAppAssignment2

---
+++ Assignment2/Assets/StartFlight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartFlight : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public GameObject counterText;
+    public int a = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText.GetComponent<Text>().text = "Counter: " + a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        a += 1;
+        counterText.GetComponent<Text>().text = "Counter: " + a;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        Destroy(other);
+    }
+}
